7ca6e220cc1b62c674daa09e9d568ab199441111
JeremiahShafer <88599453+Take-A-Breath@users.noreply.github.com>
2023-09-19 18:08:41 -0500

    Occupied some spaces

---
+++ occupied_spaces.py
@@ -0,0 +1,9 @@
+n = int(input)
+yesterday = input()
+today = input()
+
+occupied = 0
+
+for i in range(len(yesterday)):
+    if yesterday[i] == 'C' and today[i] == 'C':
+        occupied = occupied + 1
